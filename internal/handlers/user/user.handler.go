1663e08805108713f83769f260d9abab3fdc755c
pierslabs <pedrolosasp@gmail.com>
2023-11-12 20:25:39 +0100

    add Users: uuid, create, getUser, getUsers

--- internal/handlers/user/user.handler.go
+++ internal/handlers/user/user.handler.go
@@ -2,38 +2,86 @@ package userHandlers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	"github.com/pierslabs/rest-api-go/internal/dbPostgres"
 	"github.com/pierslabs/rest-api-go/internal/models"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
-
-type Response struct {
-	Data string `json:"data"`
-}
 
 func setHeaderContentTypeJson(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	var users []models.User
+
+	getAllUsers := dbPostgres.DB.Find(&users)
+
+	err := getAllUsers.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		setHeaderContentTypeJson(w)
+		w.Write([]byte("Error get users"))
+	}
 	setHeaderContentTypeJson(w)
-	w.Write([]byte("get users"))
+	json.NewEncoder(w).Encode(users)
+
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID := vars["id"]
+
+	var user models.User
+
+	getUser := dbPostgres.DB.Where("id = ? ", userID).First(&user)
+
+	if err := getUser.Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			w.WriteHeader(http.StatusNotFound)
+			setHeaderContentTypeJson(w)
+			w.Write([]byte("User not found"))
+			return
+		}
+
+		w.WriteHeader(http.StatusInternalServerError)
+		setHeaderContentTypeJson(w)
+		w.Write([]byte("Error retrieving user"))
+		return
+	}
+
+	// Si el usuario fue encontrado, devolver los detalles del usuario en la respuesta JSON
 	w.WriteHeader(http.StatusOK)
 	setHeaderContentTypeJson(w)
-	w.Write([]byte("get user"))
+	json.NewEncoder(w).Encode(user)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-
 	var user models.User
+	id := uuid.New()
+
 	json.NewDecoder(r.Body).Decode(&user)
 
-	creatUser := dbPostgres.DB.Create(&user)
+	hashPassword, err := HashPassword(user.Password)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		setHeaderContentTypeJson(w)
+		w.Write([]byte("Error hash password"))
+	}
+
+	creatUser := dbPostgres.DB.Create(&models.User{
+		ID:       id,
+		Username: user.Username,
+		Password: hashPassword,
+		Email:    user.Email,
+	})
 
 	if creatUser.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -41,6 +89,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Error create user"))
 	}
 	w.WriteHeader(http.StatusOK)
+
 	json.NewEncoder(w).Encode(user)
 }
 
@@ -54,4 +103,14 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	setHeaderContentTypeJson(w)
 	w.Write([]byte("delete user"))
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -1,12 +1,15 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 
-	ID       uint   `gorm:"primaryKey"`
-	Username string `gorm:"not null"`
-	Email    string `gorm:"not null;unique"`
-	Password string `gorm:"not null"`
+	ID       uuid.UUID `gorm:"primaryKey"`
+	Username string    `gorm:"not null"`
+	Email    string    `gorm:"not null;unique"`
+	Password string    `gorm:"not null"`
 }
